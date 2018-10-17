87062ea893c3affd89672abf0c80dd55361b0317
Reka <rekapap28@gmail.com>
2018-10-17 23:36:32 +0100

    hit points user story 2

--- spec/features/hit_points_spec.rb
+++ spec/features/hit_points_spec.rb
@@ -3,4 +3,8 @@ feature 'Hit pointts' do
     sign_in_and_play
     expect(page).to have_content 'The Rock: 60HP'
   end
+  scenario "see Player 1's Hit Points" do
+    sign_in_and_play
+    expect(page).to have_content 'Vin Diesel: 60HP'
+  end
 end

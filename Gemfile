36938a5aa06e12277bb907055c421fadd1a4d535
Reka <rekapap28@gmail.com>
2018-10-17 19:52:08 +0100

    infrastructure

--- Gemfile
+++ Gemfile
@@ -3,6 +3,7 @@ source 'https://rubygems.org'
 ruby '2.5.1'
 
 gem 'capybara'
+gem 'rspec'
 gem 'selenium-webdriver'
 gem 'sinatra'
 gem 'shotgun'
--- app.rb
+++ app.rb
@@ -1,6 +1,10 @@
 # myapp.rb
 require 'sinatra'
 
-get '/' do
-
+# Battle
+class Battle < Sinatra::Base
+  get '/' do
+    'Testing infrastructure working!'
+  end
+  run! if app_file == $PROGRAM_NAME
 end
